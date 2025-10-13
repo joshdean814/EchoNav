42690e9dbd42fe46b5064ed88c30446dfd3106ab
joshdean <u1313908@umail.utah.edu>
2025-10-13 15:44:40 +0200

    update test

--- angle_capture/test.py
+++ angle_capture/test.py
@@ -7,26 +7,24 @@ mpu6050 = mpu6050.mpu6050(0x68)
 # Define a function to read the sensor data
 def read_sensor_data():
     # Read the accelerometer values
-    accelerometer_data = mpu6050.get_accel_data()
+    # accelerometer_data = mpu6050.get_accel_data()
 
     # Read the gyroscope values
     gyroscope_data = mpu6050.get_gyro_data()
 
     # Read temp
-    temperature = mpu6050.get_temp()
+    # temperature = mpu6050.get_temp()
 
-    return accelerometer_data, gyroscope_data, temperature
+    return gyroscope_data
 
 # Start a while loop to continuously read the sensor data
 while True:
 
     # Read the sensor data
-    accelerometer_data, gyroscope_data, temperature = read_sensor_data()
+    gyroscope_data = read_sensor_data()
 
     # Print the sensor data
-    print("Accelerometer data:", accelerometer_data)
     print("Gyroscope data:", gyroscope_data)
-    print("Temp:", temperature)
 
     # Wait for 1 second
     time.sleep(1)
