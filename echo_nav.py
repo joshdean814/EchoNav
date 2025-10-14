00fd7bb6213191a2652bfd8a2307df916c3436ac
joshdean <u1313908@umail.utah.edu>
2025-10-14 21:30:54 +0200

    added button push recognition

--- echo_nav.py
+++ echo_nav.py
@@ -1,35 +1,51 @@
 from time import sleep
+from sense_hat import SenseHat
+
 from speaker_beep import SpeakerBeep
 from ultrasonic_capture import UltrasonicCapture
 
 class EchoNav():
     def __init__(self):
         self._debug = True
+        self.active = False
         self._ultrason_cap = UltrasonicCapture(debug=self._debug)
         self._speaker_beep = SpeakerBeep()
 
     def start(self) -> None:
         if self._debug:
             print("Starting up EchoNav...")
-        try:
-            while True:
-                readings = self._ultrason_cap.read_all()
-                if self._debug:
-                    print(f"[DEBUG] Readings: {readings}")
-                self._speaker_beep.update_closest(readings)
-                sleep(5)
-                
-        except KeyboardInterrupt:
-            if self._debug:
-                print("Shutting down EchoNav...")
-            self._ultrason_cap.shutdown()
-            self._speaker_beep.stop_beep()
-            exit(0)       
 
+        while self.active:
+            readings = self._ultrason_cap.read_all()
+            if self._debug:
+                print(f"[DEBUG] Readings: {readings}")
+            self._speaker_beep.update_closest(readings)
+
+    def stop(self) -> None:
+        if self._debug:
+            print("Stopping EchoNav...")
+        self._speaker_beep.stop_beep()
+
+    def shutdown(self) -> None:
+        if self._debug:
+            print("Shutting down EchoNav...")
+        self._ultrason_cap.shutdown()
+        self._speaker_beep.stop_beep()
+                
 def main():
     echo_nav = EchoNav()
-    echo_nav.start()
-
+    sense = SenseHat()
+    try:
+        while True:
+            for event in sense.stick.get_events():
+                if event.action == "pressed" and event.direction == "middle":
+                    if echo_nav.active:
+                        echo_nav.start()
+                    else:
+                        echo_nav.stop()
+    except KeyboardInterrupt:
+        echo_nav.shutdown()
+        exit(0)
 
 if __name__=="__main__":
     main()
