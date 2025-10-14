780f2e91f0ed9b2bb583c64f9b0fa7caf0fff8bd
joshdean <u1313908@umail.utah.edu>
2025-10-14 22:13:57 +0200

    main loop using mulit-threading

--- echo_nav.py
+++ echo_nav.py
@@ -1,36 +1,48 @@
 from time import sleep
 from sense_hat import SenseHat
+import threading
+from typing import Optional
 
 from speaker_beep import SpeakerBeep
 from ultrasonic_capture import UltrasonicCapture
 
 class EchoNav():
     def __init__(self):
-        self._debug = True
-        self.active = False
+        self._debug: bool = True
+        self.active: bool = False
+        self._thread: Optional[threading.Thread] = None
         self._ultrason_cap = UltrasonicCapture(debug=self._debug)
         self._speaker_beep = SpeakerBeep()
 
-    def start(self) -> None:
+    def _control_loop(self):
         if self._debug:
-            print("Starting up EchoNav...")
-
+            print("Starting up EchoNav loop...")
         while self.active:
             readings = self._ultrason_cap.read_all()
             if self._debug:
                 print(f"[DEBUG] Readings: {readings}")
             self._speaker_beep.update_closest(readings)
-
-    def stop(self) -> None:
         if self._debug:
-            print("Stopping EchoNav...")
+            print("EchoNav loop exited")
+
+    def start(self):
+        if self.active:
+            return  # already running
+        self.active = True
+        self._thread = threading.Thread(target=self._loop, daemon=True)
+        self._thread.start()
+
+    def stop(self):
+        if not self.active:
+            return
+        self.active = False
         self._speaker_beep.stop_beep()
 
     def shutdown(self) -> None:
         if self._debug:
             print("Shutting down EchoNav...")
+        self.stop()
         self._ultrason_cap.shutdown()
-        self._speaker_beep.stop_beep()
                 
 def main():
     echo_nav = EchoNav()
