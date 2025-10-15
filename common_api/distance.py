c44ee6650484e1c9f9bf567ecb308e3d4881b5bb
Josh <josh.dean31@yahoo.com>
2025-10-16 01:59:43 +0200

    fixed pin assignments

--- common_api/distance.py
+++ common_api/distance.py
@@ -6,8 +6,9 @@ from datetime import datetime
 class CarCorner(IntEnum):
     BACK_LEFT = 0
     BACK_RIGHT = 1
-    #FRONT_LEFT = 2
-    #FRONT_RIGHT = 3
+    FRONT_RIGHT = 2
+    FRONT_LEFT = 3
+    
     
     @property
     def print_name(self) -> str:
@@ -17,7 +18,9 @@ class CarCorner(IntEnum):
     def pins(self) -> Tuple[int]:
         pin_map = {
             self.BACK_RIGHT : (17, 27),
-            self.BACK_LEFT : (16, 26)
+            self.BACK_LEFT : (16, 26),
+            self.FRONT_RIGHT : (5, 6), 
+            self.FRONT_LEFT : (20, 21)
         }
         return pin_map.get(self)
     
